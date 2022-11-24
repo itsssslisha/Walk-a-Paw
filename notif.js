79395c70547fbdcceeae3a4f06018bf1398e0fe5
Shivanie <shivanie_kemraj@hotmail.com>
2022-11-24 02:33:31 -0500

    Added validation to form for toast

--- notif.js
+++ notif.js
@@ -2,26 +2,64 @@ const sum_button = document.getElementById('submit')
 const reset_button = document.getElementById('reset')
 const toasts = document.getElementById('toasts')
 
+let checker = true;
+
+function checkForm() {
+    var inputs = document.getElementsByTagName('input');
+    var nextinputs = document.getElementsByTagName('textarea');
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].hasAttribute("required")) {
+            if (inputs[i].value.length == 0) {
+                checker = false;
+                return;
+            }
+        }
+    }
+    for (var i = 0; i < nextinputs.length; i++) {
+        if (nextinputs[i].hasAttribute("required")) {
+            if (nextinputs[i].value.length == 0) {
+                checker = false;
+                return;
+            }
+        }
+    }
+}
+
 const messages = [
     'Thank you for submitting. Someone will get back to you shortly',
-    'Form Cleared!'
+    'Form Cleared!',
+    'Error! A field is left blank.'
 ]
 
-sum_button.addEventListener('click', ()=> createNotification(messages[0]))
-reset_button.addEventListener('click', ()=> createNotification(messages[1]))
+sum_button.addEventListener('click', () => submitFunction())
+reset_button.addEventListener('click', () => resetFunction())
 
-function createNotification (message = null) {
-    // if (checker == false){
-    //    return; 
-    // }
+function submitFunction() {
+    checker = true;
+    checkForm();
+    createNotification(messages[0]);
+}
+
+function resetFunction() {
+    checker = true;
+    createNotification(messages[1]);
+}
+
+
+function createNotification(message = null) {
+
     const notif = document.createElement('div')
     notif.classList.add('toast')
 
     notif.innerText = message
 
+    if (checker == false) {
+        notif.innerText = messages[2]
+    }
+
     toasts.appendChild(notif)
 
-    setTimeout(()=>{
+    setTimeout(() => {
         notif.remove()
     }, 3000)
 }
