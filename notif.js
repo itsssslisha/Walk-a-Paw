93eff0b3a6d75fe30f197440930aa73d878966bb
1rajveer1 <rajveer9905@gmail.com>
2022-11-24 01:36:29 -0500

    Updated Contacts page

--- notif.js
+++ notif.js
@@ -1,28 +1,27 @@
-const button = document.getElementById('button')
+const sum_button = document.getElementById('submit')
+const reset_button = document.getElementById('reset')
 const toasts = document.getElementById('toasts')
 
 const messages = [
-    'message 1',
-    'message 2',
-    'message 3',
-    'message 4',
+    'Thank you for submitting. Someone will get back to you shortly',
+    'Form Cleared!'
 ]
 
-button.addEventListener('click', ()=> createNotification())
-function createNotification () {
-    console.log("test")
+sum_button.addEventListener('click', ()=> createNotification(messages[0]))
+reset_button.addEventListener('click', ()=> createNotification(messages[1]))
+
+function createNotification (message = null) {
+    // if (checker == false){
+    //    return; 
+    // }
     const notif = document.createElement('div')
     notif.classList.add('toast')
 
-    notif.innerText = getRandomMessage()
-    
+    notif.innerText = message
+
     toasts.appendChild(notif)
 
     setTimeout(()=>{
         notif.remove()
     }, 3000)
-}
-
-function getRandomMessage () {
-    return messages[Math.floor.random() * messages.length]
 }
