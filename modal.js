689a10037e38a8a28af5abc4af912c7656cf3707
Shivanie <shivanie_kemraj@hotmail.com>
2022-11-26 01:20:38 -0500

    Copy Prices to main

---
+++ modal.js
@@ -0,0 +1,22 @@
+// Get the modal
+function modal(element) {
+    var modalId = element + 'Modal'
+
+    var modal = document.getElementById(modalId);
+
+    var btn = document.getElementById(element);
+    modal.style.display = "block";
+
+    close = element + 'Close'
+    var span = document.getElementsByClassName(close)[0];
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
