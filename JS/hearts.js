cfda1c59275a475265d5a49c75fb1b027af0ec99
Shivanie <shivanie_kemraj@hotmail.com>
2022-11-27 19:52:07 -0500

    Connecting Home

--- JS/hearts.js
+++ JS/hearts.js
@@ -1,25 +1,4 @@
-let slideIndex = 0;
-showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
-
-
-// class= circle 
 const loveMe = document.querySelectorAll('.loveMe')
 
 for (const luv of loveMe) {
